4ca0ffb112142db2353adc74fd5573a041a484c3
Nguyen Minh Khai <95909601+khaiv52@users.noreply.github.com>
2024-05-16 16:35:59 +0700

    Delete home_section.php

--- home_section.php
+++
@@ -1,9 +0,0 @@
-<!-- home section starts -->
-<section class="home" id="home">
-        <div class="content">
-            <h3>Các sản phẩm tươi và <span>bổ dưỡng</span> cho bạn</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde sint ad.</p>
-            <a href="#" class="btn">Shop now</a>
-        </div>
-    </section>
-    <!-- home section ends -->
